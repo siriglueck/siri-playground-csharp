bd3b4c0421088f545d4284848156c090d6b2db8c
SiriGlück <siriluck.rattanapong@gmail.com>
2025-12-16 09:55:22 +0100

    rename projects after convention

---
+++ udm-csharp-wpf-dotnet-elemente/WpfXamlGridButton/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfXamlGridButton
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Grid grid = new Grid();
+            //this.Content = grid;
+            //Button button = new Button();
+            //button.Content = "Click Me";
+            //button.FontSize = 24;
+
+            //WrapPanel wrapPanel = new WrapPanel();
+            //TextBlock textBlock1 = new TextBlock();
+            //textBlock1.Text = "a textblock";
+            //textBlock1.Foreground = Brushes.Aquamarine;
+            //wrapPanel.Children.Add(textBlock1);
+
+            //textBlock1 = new TextBlock();
+            //textBlock1.Text = "another textblock";
+            //textBlock1.Foreground = Brushes.Coral;
+            //wrapPanel.Children.Add(textBlock1);
+
+            //button.Content = wrapPanel;
+            //grid.Children.Add(button);
+
+        }
+    }
+}
